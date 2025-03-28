f49f56b44a11ddaf93c6998624cb050a239f29f8
OTKUSteyler <90369564+OTKUSteyler@users.noreply.github.com>
2025-03-28 09:59:32 +0100

    Update Settings.tsx

--- src/Settings.tsx
+++ src/Settings.tsx
@@ -2,24 +2,17 @@ import { storage } from "@vendetta/plugin";
 import { useProxy } from "@vendetta/storage";
 import { General } from "@vendetta/ui/components";
 
-const { FormSwitchRow, FormInput } = General;
+const { FormInput } = General;
 
 export default function Settings() {
     useProxy(storage);
 
     return (
-        <>
-            <FormSwitchRow
-                label="Enable Auto-Append"
-                value={storage.settings.enabled}
-                onValueChange={(value) => (storage.settings.enabled = value)}
-            />
-            <FormInput
-                title="Custom Text"
-                placeholder="Enter your custom message"
-                value={storage.settings.customText}
-                onChange={(value) => (storage.settings.customText = value)}
-            />
-        </>
+        <FormInput
+            title="Custom Text"
+            placeholder="Enter your custom message"
+            value={storage.customText}
+            onChange={(value) => (storage.customText = value)}
+        />
     );
 }
